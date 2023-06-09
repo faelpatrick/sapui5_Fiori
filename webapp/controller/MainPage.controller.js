8bf22ce762476bc32844cfe51198a1afe927aa87
faelpatrick <faelpatrick@gmail.com>
2023-06-09 11:24:16 +0100

    pagination fixed

--- webapp/controller/MainPage.controller.js
+++ webapp/controller/MainPage.controller.js
@@ -1,69 +1,76 @@
-sap.ui.require(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/ui/model/Sorter",
-  ],
-  function (
-    Controller,
-    MessageToast,
-    JSONModel,
-    Filter,
-    FilterOperator,
-    Sorter
-  ) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel", "sap/ui/model/Filter", "sap/ui/model/FilterOperator", "sap/ui/model/Sorter"],
+  function (Controller, MessageToast, JSONModel, Filter, FilterOperator, Sorter) {
     "use strict";
 
-    var iPageSize = 3;
-    var iCurrentPage = 1;
-    var oModel;
-
     return Controller.extend("my.app.controller.MainPage", {
+      // Método de inicialização do controlador
       onInit: function () {
-        oModel = new JSONModel();
+        // Carregar o modelo de dados
+        var oView = this.getView();
+        var oModel = new JSONModel();
         oModel.loadData("products.json");
-        oModel.setSizeLimit(iPageSize);
-        this.getView().setModel(oModel);
-        this.updatePageData();
+        oView.setModel(oModel);
+
+        // Manipular a conclusão da requisição do modelo
+        oModel.attachRequestCompleted(function () {
+          // Obter o número total de itens e exibir uma mensagem
+          var oBinding = oView.byId("productTable").getBinding("items");
+          var iTotalItems = oBinding.getLength();
+          MessageToast.show("Total de itens: " + iTotalItems);
+          this.iTotalItems = iTotalItems;
+          this.applyPagination();
+        }, this);
+
+        // Configurações iniciais da paginação
+        this.iPageSize = 5;
+        this.iCurrentPage = 1;
       },
 
-      updatePageData: function () {
+      // Aplicar a lógica de paginação
+      applyPagination: function () {
         var oTable = this.getView().byId("productTable");
         var oBinding = oTable.getBinding("items");
+        var iStartIndex = (this.iCurrentPage - 1) * this.iPageSize;
+        var iEndIndex = iStartIndex + this.iPageSize;
 
-        var iStartIndex = (iCurrentPage - 1) * iPageSize;
-        var iEndIndex = iStartIndex + iPageSize;
+        // Limpar os filtros existentes
+        oBinding.filter([]);
 
-        oBinding.getContexts(iStartIndex, iEndIndex);
+        // Aplicar a filtragem para exibir os itens corretos na página atual
+        if (iStartIndex < this.iTotalItems) {
+          var aFilters = [];
+          iStartIndex != 0 ? iStartIndex++ : (iStartIndex = 0);
+          for (var i = iStartIndex; i <= iEndIndex && i <= this.iTotalItems; i++) {
+            aFilters.push(new Filter("id", FilterOperator.EQ, i));
+          }
+          oBinding.filter(aFilters);
+        }
       },
 
+      // Avançar para a próxima página
       nextPage: function () {
-        var oTable = this.getView().byId("productTable");
-        var oBinding = oTable.getBinding("items");
-        var iTotalItems = oBinding.getLength();
-        var iTotalPages = Math.ceil(iTotalItems / iPageSize);
-        if (iCurrentPage < iTotalPages) {
-          iCurrentPage++;
-          this.updatePageData();
+        var iTotalPages = Math.ceil(this.iTotalItems / this.iPageSize);
+        if (this.iCurrentPage < iTotalPages) {
+          this.iCurrentPage++;
+          this.applyPagination();
         }
       },
 
+      // Voltar para a página anterior
       previousPage: function () {
-        if (iCurrentPage > 1) {
-          iCurrentPage--;
-          this.updatePageData();
+        if (this.iCurrentPage > 1) {
+          this.iCurrentPage--;
+          this.applyPagination();
         }
       },
 
+      // Executar a pesquisa de itens
       onSearch: function (event) {
-        MessageToast.show("Buscando!");
-        var searchTerm = event.getParameter("query");
         var oTable = this.getView().byId("productTable");
         var oBinding = oTable.getBinding("items");
-
+        var searchTerm = event.getParameter("query");
+        // Aplicar o filtro de pesquisa ou limpar os filtros
         if (searchTerm) {
           var oFilter = new Filter("name", FilterOperator.Contains, searchTerm);
           oBinding.filter([oFilter]);
@@ -71,29 +78,25 @@ sap.ui.require(
           oBinding.filter([]);
         }
 
-        iCurrentPage = 1; // Reinicia a página para a primeira página após a pesquisa
-        this.updatePageData();
+        // Reiniciar a página para a primeira página
+        this.iCurrentPage = 1;
+        
       },
 
+      // Ordenar a tabela
       onSort: function (event) {
         event.preventDefault();
-
         var columnName = event.getSource().getText();
         var oTable = this.getView().byId("productTable");
         var oBinding = oTable.getBinding("items");
 
+        // Determinar a ordem de classificação e aplicar a ordenação
         if (columnName) {
           var bDescending = !oBinding.aSorters[0]?.bDescending || false;
           var oSorter = new Sorter(columnName, bDescending);
           oBinding.sort([oSorter]);
 
-          MessageToast.show(
-            "Ordenando por: " +
-              columnName +
-              " (ordem " +
-              (bDescending ? "descendente" : "ascendente") +
-              ")"
-          );
+          MessageToast.show("Ordenando por: " + columnName + " (ordem " + (bDescending ? "descendente" : "ascendente") + ")");
         }
       },
     });
